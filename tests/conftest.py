03ea7ebfbea8281d125c583f7fdecffd83978906
ilya <muravjovilya@gmail.com>
2022-10-02 17:47:06 +0300

    Use separate json file for each test function

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,20 +7,22 @@ def pytest_generate_tests(metafunc):
     if "config_data" not in metafunc.fixturenames:
         return
     test_file_path = pathlib.Path(metafunc.module.__file__)
-    test_file_config = (
-        test_file_path.parent / "resources" / test_file_path.name
+    test_config = (
+        test_file_path.parent
+        / "resources"
+        / test_file_path.with_suffix("").name
+        / metafunc.function.__name__
     ).with_suffix(".json")
-    test_file_data = json.loads(test_file_config.read_text())
-    test_function_data = test_file_data.get(metafunc.function.__name__)
-    if not isinstance(test_function_data, list):
+    test_config_content = json.loads(test_config.read_text())
+    if not isinstance(test_config_content, list):
         raise Exception(
             f"config_data for {metafunc.function.__name__} in {metafunc.module.__file__} isn't a list"
         )
     metafunc.parametrize(
         "config_data",
-        test_function_data,
+        test_config_content,
         ids=(
             textwrap.shorten(config_data.__str__(), 100, placeholder="...")
-            for config_data in test_function_data
+            for config_data in test_config_content
         ),
     )

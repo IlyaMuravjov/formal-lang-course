35c16b084d0d39fcb67b80a80cc6ade3cceb3a36
ilya <muravjovilya@gmail.com>
2022-10-02 22:43:43 +0300

    Solve task 4

--- tests/test_regular_path_query.py
+++ tests/test_regular_path_query.py
@@ -1,9 +1,9 @@
-from project.regular_path_query import regular_path_query
+from project.regular_path_query import regular_path_query_all_pairs
 from tests.utils import read_graph
 
 
-def test_regular_path_query(config_data: dict):
-    assert regular_path_query(
+def test_regular_path_query_all_pairs(config_data: dict):
+    assert regular_path_query_all_pairs(
         regex=config_data["regex"],
         graph=read_graph(config_data["graph"]),
         start_states=set(config_data["start-states"])

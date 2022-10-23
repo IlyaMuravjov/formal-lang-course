b974d66c147713b2523e487d4d31e5a77e3b2084
ilya <muravjovilya@gmail.com>
2022-10-23 19:27:16 +0300

    Solve task 6

---
+++ project/cfg_utils.py
@@ -0,0 +1,61 @@
+import json
+from os import PathLike
+
+import pyformlang.cfg
+
+__all__ = ["cfg_to_weak_normal_form", "json_to_cfg", "read_cfg"]
+
+
+def cfg_to_weak_normal_form(cfg: pyformlang.cfg.CFG) -> pyformlang.cfg.CFG:
+    # temporarily replace epsilon with a placeholder terminal
+    # so `to_normal_form()` works as `to_weak_normal_form()`
+    epsilon_placeholder = pyformlang.cfg.Terminal("CFG#EpsilonPlaceholder")
+    while epsilon_placeholder in cfg.terminals or epsilon_placeholder in cfg.variables:
+        epsilon_placeholder = pyformlang.cfg.Terminal(epsilon_placeholder.value + "_")
+    cfg = pyformlang.cfg.CFG(
+        start_symbol=cfg.start_symbol,
+        productions={
+            pyformlang.cfg.Production(
+                production.head,
+                [
+                    epsilon_placeholder
+                    if isinstance(cfg_object, pyformlang.cfg.Epsilon)
+                    else cfg_object
+                    for cfg_object in production.body
+                ]
+                if len(production.body) > 0
+                else [epsilon_placeholder],
+            )
+            for production in cfg.productions
+        },
+    )
+    cfg = cfg.to_normal_form()
+    return pyformlang.cfg.CFG(
+        start_symbol=cfg.start_symbol,
+        productions={
+            pyformlang.cfg.Production(
+                production.head,
+                [
+                    pyformlang.cfg.Epsilon()
+                    if cfg_object == epsilon_placeholder
+                    else cfg_object
+                    for cfg_object in production.body
+                ],
+            )
+            for production in cfg.productions
+        },
+    )
+
+
+def json_to_cfg(data: dict) -> pyformlang.cfg.CFG:
+    return pyformlang.cfg.CFG.from_text(
+        start_symbol=pyformlang.cfg.Variable(
+            data["start"] if "start" in data else None
+        ),
+        text=data["productions"] if "productions" in data else "",
+    )
+
+
+def read_cfg(path: PathLike) -> pyformlang.cfg.CFG:
+    with open(path, "r") as fs:
+        return json_to_cfg(json.load(fs))

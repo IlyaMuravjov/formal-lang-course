5ff80e2bb867538c210f7b1480aae2afc084d152
ilya <muravjovilya@gmail.com>
2022-10-30 20:38:16 +0300

    Solve task 7

---
+++ project/ecfg.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import Dict
+
+import pyformlang.cfg
+
+__all__ = ["ECFG"]
+
+
+@dataclass
+class ECFG:
+    start_symbol: pyformlang.cfg.Variable
+    productions: Dict[pyformlang.cfg.Variable, pyformlang.regular_expression.Regex]
+
+    @staticmethod
+    def from_cfg(cfg: pyformlang.cfg.CFG) -> "ECFG":
+        new_productions: Dict[
+            pyformlang.cfg.Variable, pyformlang.regular_expression.Regex
+        ] = dict()
+        for production in cfg.productions:
+            regex = pyformlang.regular_expression.Regex(
+                " ".join(
+                    "$" if isinstance(obj, pyformlang.cfg.Epsilon) else obj.value
+                    for obj in production.body
+                )
+                if len(production.body) > 0
+                else "$"
+            )
+            new_productions[production.head] = (
+                new_productions[production.head].union(regex)
+                if production.head in new_productions
+                else regex
+            )
+        return ECFG(start_symbol=cfg.start_symbol, productions=new_productions)
+
+    @staticmethod
+    def from_data(data: dict):
+        return ECFG(
+            start_symbol=pyformlang.cfg.Variable(
+                data["start"] if "start" in data else None
+            ),
+            productions={
+                pyformlang.cfg.Variable(var): pyformlang.regular_expression.Regex(regex)
+                for var, regex in data["productions"].items()
+            }
+            if "productions" in data
+            else dict(),
+        )
---
+++ project/rsm.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from typing import Dict
+
+import pyformlang
+
+from project.ecfg import ECFG
+from project.fa_utils import nfa_from_data
+
+__all__ = ["RSM"]
+
+
+@dataclass
+class RSM:
+    start_symbol: pyformlang.cfg.Variable
+    boxes: Dict[pyformlang.cfg.Variable, pyformlang.finite_automaton.EpsilonNFA]
+
+    def minimize(self) -> "RSM":
+        minimized_rsm = RSM(start_symbol=self.start_symbol, boxes=dict())
+        for var, fa in self.boxes.items():
+            minimized_rsm.boxes[var] = fa.minimize()
+        return minimized_rsm
+
+    @staticmethod
+    def from_ecfg(ecfg: ECFG) -> "RSM":
+        return RSM(
+            start_symbol=ecfg.start_symbol,
+            boxes={
+                var: regex.to_epsilon_nfa() for var, regex in ecfg.productions.items()
+            },
+        )
+
+    @staticmethod
+    def from_data(data: dict) -> "RSM":
+        return RSM(
+            start_symbol=pyformlang.cfg.Variable(
+                data["start"] if "start" in data else None
+            ),
+            boxes={
+                pyformlang.cfg.Variable(var): nfa_from_data(fa)
+                for var, fa in data["boxes"].items()
+            }
+            if "boxes" in data
+            else dict(),
+        )

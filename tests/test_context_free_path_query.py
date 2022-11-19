98936dd62afa0cccf87f08cf17ae342732989ecc
ilya <muravjovilya@gmail.com>
2022-11-19 20:41:39 +0300

    Test task 10 solution

--- tests/test_context_free_path_query.py
+++ tests/test_context_free_path_query.py
@@ -1,10 +1,11 @@
 from project.cfg_utils import cfg_from_data
 from project.context_free_path_query import filtered_cfpq_with_hellings
+from project.context_free_path_query import filtered_cfpq_with_matrix
 from project.graph_utils import graph_from_data
 
 
-def test_filtered_cfpq_with_hellings(config_data: dict):
-    assert filtered_cfpq_with_hellings(
+def _test_filtered_cfpq_impl(impl, config_data: dict):
+    assert impl(
         cfg_from_data(config_data["cfg"]),
         graph_from_data(config_data["graph"]),
         start_nodes=set(config_data["start-nodes"])
@@ -14,3 +15,11 @@ def test_filtered_cfpq_with_hellings(config_data: dict):
         if "final-nodes" in config_data
         else None,
     ) == {(start, finish) for (start, finish) in config_data["expected-result"]}
+
+
+def test_filtered_cfpq_with_hellings(config_data: dict):
+    _test_filtered_cfpq_impl(filtered_cfpq_with_hellings, config_data)
+
+
+def test_filtered_cfpq_with_matrix(config_data: dict):
+    _test_filtered_cfpq_impl(filtered_cfpq_with_matrix, config_data)
